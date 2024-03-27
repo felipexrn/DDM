5ca85ce7d4e391a811493c7c14716417d0deaced
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2024-03-27 00:07:36 -0300

    correção do caminho das imagens

--- qajuda_main.dart
+++ qajuda_main.dart
@@ -15,12 +15,12 @@ class MyApp extends StatelessWidget {
 
   // Lista de URLs de imagens distintas
   static const List<String> imageUrls = [
-    'https://raw.githubusercontent.com/felipexrn/DDM/imagens/main/alfabetizacao.jpg',
-    'https://raw.githubusercontent.com/felipexrn/DDM/imagens/main/animais.jpg',
-    'https://raw.githubusercontent.com/felipexrn/DDM/imagens/main/limpeza-praia2.jpg',
-    'https://raw.githubusercontent.com/felipexrn/DDM/imagens/main/plantacao.jpg',
-    'https://raw.githubusercontent.com/felipexrn/DDM/imagens/main/roupa.jpg',
-    'https://raw.githubusercontent.com/felipexrn/DDM/imagens/main/alfabetizacao-pcd.jpg',
+    'https://raw.githubusercontent.com/felipexrn/DDM/main/imagens/alfabetizacao.jpg',
+    'https://raw.githubusercontent.com/felipexrn/DDM/main/imagens/animais.jpg',
+    'https://raw.githubusercontent.com/felipexrn/DDM/main/imagens/limpeza-praia2.jpg',
+    'https://raw.githubusercontent.com/felipexrn/DDM/main/imagens/plantacao.jpg',
+    'https://raw.githubusercontent.com/felipexrn/DDM/main/imagens/roupa.jpg',
+    'https://raw.githubusercontent.com/felipexrn/DDM/main/imagens/alfabetizacao-pcd.jpg',
   ]; 
   // Lista de legenda de imagens distintas
   static const List<String> lengedasImagens = [
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(
                   height: 150,
                   child: Image.network(
-                      'https://raw.githubusercontent.com/felipexrn/DDM/imagens/main/logoqajuda.png'),
+                      'https://raw.githubusercontent.com/felipexrn/DDM/main/imagens/logoqajuda.png'),
                 ),
                 SizedBox(
                   height: 50, // Espaço entre a imagem e a caixa de busca
